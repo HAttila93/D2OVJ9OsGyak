7bbe0233488918ff8f3c863817c9909fe6fa907a
HAttila93 <78981894+HAttila93@users.noreply.github.com>
2021-04-28 13:02:58 +0200

    Add files via upload

---
+++ d2ovj9_0416/child.c
@@ -0,0 +1,17 @@
+/*
+Forras:
+Dr. Vadasz Denes: Operacios rendszerek
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    for(i=0; i<5; i++){
+        printf("Hegedus Attila D2OVJ9\n");
+    }
+    return 0;
+}
